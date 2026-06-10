ff9eaac87985c813e80ea9484f389b315ae6b513
Sajjan Adhikari <257258237+sajjan-adhikari@users.noreply.github.com>
2026-06-10 19:22:06 +0545

    chore: remove node_modules, .svelte-kit, and build from tracking and add .gitignore

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CRfOQJ2V.js","_app/immutable/chunks/D39iZMBZ.js","_app/immutable/chunks/nQMPD9xd.js","_app/immutable/chunks/xihTtKlq.js"];
-export const stylesheets = [];
-export const fonts = [];
